6942d6a1ea3094656a9045c4553a03c653b39ee7
sasha <mr.skreman@gmail.com>
2024-08-17 17:22:11 +0300

    main menu is created

---
+++ core/menu.py
@@ -0,0 +1,53 @@
+from aiogram import Router, F
+from aiogram.types.message import Message
+from aiogram.types import CallbackQuery
+from aiogram.types.inline_keyboard_button import InlineKeyboardButton
+from aiogram.types.inline_keyboard_markup import InlineKeyboardMarkup
+from aiogram.filters import Command, CommandStart
+
+top_level_router = Router()
+
+
+@top_level_router.message(CommandStart())
+async def start(message: Message) -> None:
+    await message.answer('привет чтобы вызвать меню напиши /menu')
+
+
+@top_level_router.message(Command('menu'))
+async def menu(message: Message) -> None:
+
+    kb = InlineKeyboardMarkup(
+        inline_keyboard=[  # первая строка
+            [InlineKeyboardButton(text='Предсказать результат матча', callback_data='predict'),
+             InlineKeyboardButton(text='Предсказать лайв матчи', callback_data='live_predict')],
+            # вторая строка
+            [InlineKeyboardButton(text='Предсказать результаты матчей на сегодня',
+                                  callback_data='today_predict'),
+                InlineKeyboardButton(text='Предсказать результаты матчей на завтра',
+                                     callback_data='tommorow_predict')]])
+
+    await message.answer('reply keyboard', reply_markup=kb)
+
+
+@top_level_router.callback_query(F.data == 'predict')
+async def predict(callback: CallbackQuery) -> None:
+    await callback.answer()
+    await callback.message.answer('predict')
+
+
+@top_level_router.callback_query(F.data == 'live_predict')
+async def live_predict(callback: CallbackQuery) -> None:
+    await callback.answer()
+    await callback.message.answer('live_predict')
+
+
+@top_level_router.callback_query(F.data == 'today_predict')
+async def today_predict(callback: CallbackQuery) -> None:
+    await callback.answer()
+    await callback.message.answer('today_predict')
+
+
+@top_level_router.callback_query(F.data == 'tommorow_predict')
+async def tommorow_predict(callback: CallbackQuery) -> None:
+    await callback.answer()
+    await callback.message.answer('tommorow_predict')
--- main.py
+++ main.py
@@ -1,19 +1,20 @@
 import os
 import asyncio
+import logging
+from core.menu import top_level_router
 from dotenv import load_dotenv
 from aiogram import Bot, Dispatcher
-from aiogram.types.message import Message
-from aiogram.filters import Command, CommandStart
 
 load_dotenv()
 
 TOKEN = os.getenv('TOKEN')
 dispatcher = Dispatcher()
+logger = logging.getLogger(__name__)
+logging.basicConfig(filename='hltv_v2_log.log', filemode='w')
 
+routers = [top_level_router]
 
-@dispatcher.message(CommandStart())
-async def start(message: Message) -> None:
-    await message.answer('привет')
+dispatcher.include_routers(*routers)
 
 
 async def main() -> None:
@@ -22,5 +23,4 @@ async def main() -> None:
 
 
 if __name__ == '__main__':
-    print(TOKEN)
     asyncio.run(main())
